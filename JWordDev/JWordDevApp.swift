457770d4ee2d4dffb49badd35b616fa04d46403c
yong0905 <kang203062@gmail.com>
2025-04-27 19:05:53 +0900

    Initial Commit

---
+++ JWordDev/JWordDevApp.swift
@@ -0,0 +1,17 @@
+//
+//  JWordDevApp.swift
+//  JWordDev
+//
+//  Created by 강 on 4/27/25.
+//
+
+import SwiftUI
+
+@main
+struct JWordDevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
